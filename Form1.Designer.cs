9d7368fb7fc5ca7d063628cef321eec1e95d1e92
morzzic <65243242+morzzic@users.noreply.github.com>
2023-02-04 15:53:13 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace PathConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rawDataBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rawDataBox
+            // 
+            this.rawDataBox.Location = new System.Drawing.Point(12, 25);
+            this.rawDataBox.Multiline = true;
+            this.rawDataBox.Name = "rawDataBox";
+            this.rawDataBox.Size = new System.Drawing.Size(142, 295);
+            this.rawDataBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Raw Data:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(256, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output:";
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(160, 25);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(241, 295);
+            this.outputBox.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(389, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 361);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rawDataBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Path Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox rawDataBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace PathConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                outputBox.Text = Convert(rawDataBox.Text);
+            }
+            catch (Exception ex)
+            {
+                outputBox.Text = ex.Message;
+            }
+        }
+
+        private static string Convert(string rawData)
+        {
+            rawData = rawData.Replace("GenericPropertyJSON:", string.Empty);
+            GenericProperty property = JsonConvert.DeserializeObject<GenericProperty>(rawData);
+            int size = (int)property.children[0].children[0].val;
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"new Vector2[]{Environment.NewLine}{{");
+            for (int i = 1; i < size + 1; i++)
+            {
+                sb.Append($"{Environment.NewLine}   new Vector2({property.children[0].children[i].children[0].val.ToString().Replace(',', '.')}f, {property.children[0].children[i].children[1].val.ToString().Replace(',', '.')}f)");
+                if (i != size) sb.Append(",");
+            }
+            sb.Append($"{Environment.NewLine}}};");
+            return sb.ToString();
+        }
+
+        private class GenericProperty
+        {
+            public string name;
+            public int type;
+            public int arraySize;
+            public string arrayType;
+            public Children[] children;
+        }
+
+        private class Children
+        {
+            public string name;
+            public int type;
+            public int arraySize;
+            public string arrayType;
+            public SubChildren[] children;
+            public float val;
+        }
+
+        private class SubChildren
+        {
+            public string name;
+            public int type;
+            public float val;
+            public SubChildren[] children;
+        }
+    }
+}
